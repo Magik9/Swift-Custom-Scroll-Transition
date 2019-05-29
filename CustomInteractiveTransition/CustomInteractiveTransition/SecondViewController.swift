92e3d67b41ecaafe3298ea44e2b1fa453a1127e3
Magik9 <48517324+Magik9@users.noreply.github.com>
2019-05-29 21:50:37 +0200

    Add files via upload

---
+++ CustomInteractiveTransition/CustomInteractiveTransition/SecondViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.swift
+//  WebViewSwift
+//
+//  Created by Gabriele Magi on 12/02/2019.
+//  Copyright © 2019 Gabriele Magi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+               
+           
+    }
+
+    @IBAction func click(_ sender: UIButton) {
+        
+        self.view.backgroundColor = UIColor.clear
+    }
+    
+    
+}
